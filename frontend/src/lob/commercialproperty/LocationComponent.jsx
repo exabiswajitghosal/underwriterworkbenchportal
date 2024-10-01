a86158cea5796796198f68b342b275607d26b647
Anwesha Bose <99438778+anwesha-11@users.noreply.github.com>
2024-10-01 13:36:16 +0530

    Add files via upload

---
+++ frontend/src/lob/commercialproperty/LocationComponent.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './LocationComponent.module.css';
+import {LocationHeader} from './LocationHeader';
+import {LocationTable} from './LocationTable';
+
+import Documents from '../../layout/RightSidebar';
+import {LocationForm} from '../../layout/Location';
+import { Col, Row } from 'antd';
+
+export function LocationComponent() {
+  return (
+    <Row>
+      <Col span={20}>
+    <main className={styles.mainContainer}>
+      <LocationHeader />
+      <LocationTable />
+      <LocationForm/>
+      
+    </main></Col>
+    <Col span={4}><Documents/></Col>
+       </Row>
+  );
+}
+
+export default LocationComponent;
