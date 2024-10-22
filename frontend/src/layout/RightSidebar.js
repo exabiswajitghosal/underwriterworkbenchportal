d60ae77fb3e1c240e6b612ff7cbdb350fdfb7ce9
Anwesha Bose <99438778+anwesha-11@users.noreply.github.com>
2024-10-22 11:30:24 +0530

    Add files via upload

--- frontend/src/layout/RightSidebar.js
+++ frontend/src/layout/RightSidebar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './RightSidebar.css'; // CSS file for styling
 
-
+import AIModal from './AIModal';
 const Documents = () => {
   return (
-    <div className="sidebar" >
+    <div>
+    <div className="sidebar">
       <h5 className="sidebar-header"> Documents</h5>
       <div className="sidebar-links">
         <a href="document1.pdf" target="_blank" rel="noopener noreferrer">
@@ -17,6 +18,17 @@ const Documents = () => {
           Corelogic Riskmeter
         </a>
       </div>
+    </div>
+    <div className="sidebar">
+    {/* <h8 className="sidebar-headerai" >AI</h8> */}
+      <h8 className="sidebar-header" >Suggestions</h8>
+      <div className="sidebar-links" style={{marginTop:'20px'}}>
+       
+          <AIModal/>
+       
+      
+      </div>
+    </div>
     </div>
   );
 };
